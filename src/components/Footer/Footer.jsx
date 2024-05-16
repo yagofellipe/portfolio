bb5f91291833a125e96af0e1b776dedea281ab61
yagofellipe <yagofellipe09@gmail.com>
2024-05-16 09:56:36 -0300

    update

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,17 @@
+import './footer.css'
+
+const Header = () => {
+
+  return (
+    <footer className="footer">
+
+      <p className="contact-me">
+            <b className="contact-me-text">Contact me:</b><br/>
+            <span className='email'>yagofellipe09@gmail.com</span>
+      </p>
+
+    </footer>
+  )
+}
+
+export default Header
